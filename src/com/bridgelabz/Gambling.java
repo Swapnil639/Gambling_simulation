cfa895d6c8a914ea005549658dd56459f7cd27a6
Swapnil639 <swapnilpatil631997@gmail.com>
2022-08-16 20:39:35 +0530

    Added :Start game

--- src/com/bridgelabz/Gambling.java
+++ src/com/bridgelabz/Gambling.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class Gambling {
+    static int stake_Amount= 100;
+    static int bet=1;
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Problem");
+        System.out.println("Amount of stake start game of everyday is :"+stake_Amount);
+        System.out.println("Amount to bet after every game " + bet);
+
     }
 }
